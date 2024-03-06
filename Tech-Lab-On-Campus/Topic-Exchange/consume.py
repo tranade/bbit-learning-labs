5fcb5422e22f05366faae7d2011195371266560d
Tanvi Ranade <tanviranade@gmail.com>
2024-03-05 19:41:23 -0500

    implemented consumer for topic exchange

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -23,7 +23,9 @@ def main(sector: str, queueName: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
-    
+
+
+    bindingKey = 'Stock.*.' + sector
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
     
@@ -35,5 +37,7 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
+    sector = sys.argv[0]
+    queue = sys.argv[1]
 
     sys.exit(main(sector,queue))
